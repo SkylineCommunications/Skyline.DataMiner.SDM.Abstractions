6e90cd58654f41ee59d9eca6c0a24de1d562ccfb
Arne Maes <arne.maes@skyline.be>
2026-09-18 09:02:17 +0200

    Add Type property filtering support to SDM
    Added TypeFieldConverter and registered it for System.Type in the filtering infrastructure. Updated SDM.Abstractions API with TypeFilterExtensions for Equal/NotEqual filters. Added unit tests for Type filtering scenarios.

---
+++ SDM.Abstractions/Types/Converters/TypeFieldConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace Skyline.DataMiner.SDM.Types.Converters
+{
+	using System;
+
+	internal class TypeFieldConverter : IFieldValueConverter
+	{
+		public Type FieldType { get => typeof(Type); }
+
+		public object Convert(object value)
+		{
+			if (value is null || value is Type)
+			{
+				return value;
+			}
+
+			throw new InvalidCastException($"Cannot convert value of type '{value.GetType().FullName}' to '{typeof(Type).FullName}'.");
+		}
+	}
+}
